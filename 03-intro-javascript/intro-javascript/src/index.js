a35436b0f79c37b3b967b343d91216185cac580a
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-09-29 14:24:02 -0500

    Variables y constantes

---
+++ 03-intro-javascript/intro-javascript/src/index.js
@@ -0,0 +1,16 @@
+// Lección 13. Variables y constantes
+
+const nombre = 'Víctor';
+
+let apellido = 'Vázquez';
+let valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    valorDado = 6;
+
+    console.log(valorDado);
+}
+
+console.log(valorDado);
