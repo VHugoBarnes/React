02538738914cad15e53483ed3d1a9bb438d8c9a9
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-05 23:44:24 -0500

    Async - Await

--- 03-intro-javascript/intro-javascript/src/index.js
+++ 03-intro-javascript/intro-javascript/src/index.js
@@ -1,17 +1,26 @@
-// Lección 23. Fetch API
+// Lección 24. Async - Await
 
-const apiKey = '69P2JB6qo3viyWDvHpnvtSyvEGFzNfzb';
+// Primero creamos una promesa común y corriente
+// const getImagenPromesa = () => new Promise( resolve => resolve('udemy.com') );
+// getImagenPromesa().then(console.log);
 
-const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+// Creándolo con Async
+const getImage = async() => {
+    try {
+        const apiKey = '69P2JB6qo3viyWDvHpnvtSyvEGFzNfzb';
+        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const { data } = await resp.json();
 
-peticion
-    .then(resp => resp.json())
-    .then(({data}) => {
-        const {url} = data.images.original;
-        
+        console.log( data );
+        const { url } = data.images.original;
         const img = document.createElement('img');
         img.src = url;
 
-        document.body.append(img)
-    })
-    .catch(console.warn);
+        document.body.append( img );
+    }catch(error){
+        // Manejo del error
+        console.error(error);
+    }
+}
+
+getImage().then();
