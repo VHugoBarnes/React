8cfe089d9e08afaaee6290f5a1ecab86b890e0a9
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-07 20:09:57 -0500

    Operador condicional ternario

--- 03-intro-javascript/intro-javascript/src/index.js
+++ 03-intro-javascript/intro-javascript/src/index.js
@@ -1,26 +1,16 @@
-// Lección 24. Async - Await
+// Lección 25. Operación condicional ternario
 
-// Primero creamos una promesa común y corriente
-// const getImagenPromesa = () => new Promise( resolve => resolve('udemy.com') );
-// getImagenPromesa().then(console.log);
+const activo = true;
 
-// Creándolo con Async
-const getImage = async() => {
-    try {
-        const apiKey = '69P2JB6qo3viyWDvHpnvtSyvEGFzNfzb';
-        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-        const { data } = await resp.json();
+// let mensaje = '';
+//
+// if (activo) {
+//     mensaje = 'Activo';
+// } else{ 
+//     mensaje = 'Inactivo';
+// }
 
-        console.log( data );
-        const { url } = data.images.original;
-        const img = document.createElement('img');
-        img.src = url;
+// const mensaje = (activo) ? 'Activo' : 'Inactivo';
+const mensaje = activo && 'Activo'; 
 
-        document.body.append( img );
-    }catch(error){
-        // Manejo del error
-        console.error(error);
-    }
-}
-
-getImage().then();
+console.log(mensaje);
