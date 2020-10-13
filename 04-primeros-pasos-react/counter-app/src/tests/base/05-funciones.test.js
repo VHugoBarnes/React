4ff7df09977bdced3f6affcd15882a1df8e33265
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-13 12:16:30 -0500

    Función toEqual para evaluar objetos

---
+++ 04-primeros-pasos-react/counter-app/src/tests/base/05-funciones.test.js
@@ -0,0 +1,34 @@
+import { getUser, getUsuarioActivo } from '../../base/05-funciones';
+
+describe('Pruebas en 05-funciones', () => {
+    
+    test('getUser debe retornar un objeto', () => {
+        
+        const userTest = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+
+        expect(user).toEqual(userTest);
+
+    })
+
+    // getUsuarioActivo debe retornar un objeto
+    test('getUsuarioActivo debe retornar un objeto', () => {
+
+        const nombre = 'Víctor';
+        const usuarioActivoTest = {
+            uid: 'ABC567',
+            username: nombre,
+        };
+
+        const usuarioActivo = getUsuarioActivo(nombre);
+
+        expect(usuarioActivo).toEqual(usuarioActivoTest);
+
+    })
+    
+
+})
