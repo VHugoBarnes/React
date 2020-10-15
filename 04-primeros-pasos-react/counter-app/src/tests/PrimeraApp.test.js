a10ae60c625a83efa1f9f8974f0bb0ef36373229
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-15 13:48:53 -0500

    Pruebas a componentes en React con Enzyme y snapshots en Jest

--- 04-primeros-pasos-react/counter-app/src/tests/PrimeraApp.test.js
+++ 04-primeros-pasos-react/counter-app/src/tests/PrimeraApp.test.js
@@ -1,17 +1,22 @@
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 import React from 'react';
-import { getByText, render } from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
 
 describe('Pruebas en <PrimeraApp />', () => {
     
-    test('Debe mostrar el mensaje "Hola Keko"', () => {
-        
+    // test('Debe mostrar el mensaje "Hola Keko"', () => {        
+    //     const saludo = 'Hola Keko';
+    //     const { getByText } = render(<PrimeraApp saludo={ saludo }/>);
+    //     expect( getByText(saludo) ).toBeInTheDocument();        
+    // });
+
+    test('Debe mostrar <PrimeraApp /> correctamente', () => {
         const saludo = 'Hola Keko';
+        const wrapper = shallow( <PrimeraApp saludo={ saludo } /> );
 
-        const { getByText } = render(<PrimeraApp saludo={ saludo }/>);
+        expect( wrapper ).toMatchSnapshot();
 
-        expect( getByText(saludo) ).toBeInTheDocument();
-        
     });
-    
+        
 });
