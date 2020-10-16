e891d9d88e2eda874014c4828ca6b44891c9c359
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-16 16:21:57 -0500

    Creando el componente de la aplicación, y añadiendo algunos estilos

---
+++ 06-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,13 @@
+// Lección 69. GifExpertApp component
+import React from 'react';
+
+export const GifExpertApp = () => {
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr></hr>
+        </>
+    );
+
+};
