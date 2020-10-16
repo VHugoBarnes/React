38cf6ae36021d0294301cf71478d3f0ac62e6bc9
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-16 17:38:28 -0500

    handleAdd para añadir más categorías

--- 06-gif-expert-app/src/GifExpertApp.js
+++ 06-gif-expert-app/src/GifExpertApp.js
@@ -1,12 +1,30 @@
 // Lección 69. GifExpertApp component
-import React from 'react';
+import React, {useState} from 'react';
 
 export const GifExpertApp = () => {
+
+    // const categories = ['The Office', 'Minecraft', 'Clash of Clans'];
+    const [categories, setCategories] = useState(['The Office', 'Minecraft', 'Clash of Clans']);
+
+    const handleAdd = () => {
+        // setCategories( ['Little Witch Academia', ...categories] );
+        setCategories( cats => ['Little Witch Academia', ...cats] );
+    };
 
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr></hr>
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    categories.map( category =>{
+                    return <li key={category}>{category}</li>
+                    } )
+                }
+            </ol>
         </>
     );
 
