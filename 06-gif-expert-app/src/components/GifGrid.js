72485fb7c0684d2a362e490b3ba6e856f43f1080
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-19 13:32:26 -0500

    Usando por primera vez el Hook useEffect

--- 06-gif-expert-app/src/components/GifGrid.js
+++ 06-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({category}) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect( () => {
+        getGif();
+    }, [] );
 
     const getGif = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=the+office&limit=6&api_key=69P2JB6qo3viyWDvHpnvtSyvEGFzNfzb';
@@ -19,11 +25,11 @@ export const GifGrid = ({category}) => {
 
     }
 
-    getGif();
-
     return (
         <div>
             <h3>{ category }</h3>
+            <h3>{count}</h3>
+            <button onClick={ () => setCount(count + 1) } ></button>
         </div>
     )
 }
