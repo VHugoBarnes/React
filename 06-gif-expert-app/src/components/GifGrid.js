c050f4fb3338c3b1ad4321c26d830da8378a8a86
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-19 17:36:55 -0500

    Custom Hook

--- 06-gif-expert-app/src/components/GifGrid.js
+++ 06-gif-expert-app/src/components/GifGrid.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { getGif } from '../helpers/getGif';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 // import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
     // const [images, setImages] = useState([]);
+    const { loading } = useFetchGifs();
 
     // useEffect( () => {
     //     getGif(category).then( setImages );
@@ -15,6 +17,7 @@ export const GifGrid = ({category}) => {
     return (
         <>
         <h3>{ category }</h3>
+        { loading ? 'Cargando...' : 'Fin de la carga' }
         {/* <div className="card-grid">
                 {
                     images.map( (i) => 
