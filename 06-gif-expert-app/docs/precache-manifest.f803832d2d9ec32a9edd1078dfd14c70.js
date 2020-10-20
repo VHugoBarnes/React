47c9a947906228a47427810a6e1b0e6f9d8721af
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-19 21:02:59 -0500

    Build 2

---
+++ 06-gif-expert-app/docs/precache-manifest.f803832d2d9ec32a9edd1078dfd14c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557a174282684312afba208eb403d21a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e58c463a97aadf71e46",
+    "url": "/static/css/main.d9d911a0.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e58c463a97aadf71e46",
+    "url": "/static/js/main.c86b6c82.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
