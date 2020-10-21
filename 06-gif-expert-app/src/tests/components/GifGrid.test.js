7e7d9584657ef4e35a0ba3bcf6df7d425f9be3cf
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-21 14:51:29 -0500

    Mock a customHook

---
+++ 06-gif-expert-app/src/tests/components/GifGrid.test.js
@@ -0,0 +1,39 @@
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
+import React from 'react';
+import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+
+jest.mock('../../hooks/useFetchGifs');
+
+describe('Pruebas en <GifGrid />', () => {
+    
+    const category = 'Ellie Kemper';
+
+    test('El componente debe de renderizarse', () => {
+        
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true,
+        });
+        
+        const wrapper = shallow(<GifGrid category={ category }/>);
+        expect( wrapper ).toMatchSnapshot();
+    });
+    
+    test('Debe de mostrar items cuando se cargan imágenes', () => {
+        const gifs = [{
+            id: 'ABC',
+            url: 'https://localhost/cualquier/cosa.jpg',
+            title: 'Cualquier cosa'
+        }];
+        
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false,
+        });
+        const wrapper = shallow(<GifGrid category={ category }/>);
+        expect( wrapper ).toMatchSnapshot();
+    });
+    
+});
