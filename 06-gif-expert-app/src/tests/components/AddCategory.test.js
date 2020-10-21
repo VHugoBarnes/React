6d4d95c6b208fa68b7c6066ee231869ca20b2774
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-20 22:16:08 -0500

    Más simulaciones

--- 06-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 06-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -31,6 +31,29 @@ describe('Pruebas en el componente <AddCategory />', () => {
     test('No debe de ingresar la información con submit', () => {
         wrapper.find('form').simulate('submit', { preventDefault(){} });
         expect( setCategories ).not.toHaveBeenCalled()
+    });
+    
+    test('Debe de llamar al setCategories y limpiar la caja de texto', () => {
+        /**
+         * 1. Simular el inputChange
+         * 2. Simular el submit
+         * 3. setCategories se debe de haber llamado
+         * 4. el valor del input debe de estar ''
+         */
+
+         // 1. Simular el inputChange
+         wrapper.find('input').simulate('change', {target: {value: 'Hola'}});
+
+         // 2. Simular el submit
+         wrapper.find('form').simulate('submit', {preventDefault(){}});
+
+         // 3. setCategories se debe de haber llamado
+         expect( setCategories ).toHaveBeenCalled();
+
+         // 4. El valor del input de de estar ''
+         expect( wrapper.find('input').prop('value') ).toBe('');
+
+
     })
     
 });
