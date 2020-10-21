d02010ff87f4029a41beb541134ff8e26f425f51
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-21 16:41:22 -0500

    Evaluando si existe un componente

--- 06-gif-expert-app/src/tests/components/GifGrid.test.js
+++ 06-gif-expert-app/src/tests/components/GifGrid.test.js
@@ -33,7 +33,10 @@ describe('Pruebas en <GifGrid />', () => {
             loading: false,
         });
         const wrapper = shallow(<GifGrid category={ category }/>);
+        
         expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('p').exists() ).toBe(false);
+        expect( wrapper.find('GifGridItem').length ).toBe( gifs.length );
     });
     
 });
