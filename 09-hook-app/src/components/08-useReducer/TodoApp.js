0cc358a69602234bb4e772eef18098b8342e30da
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-10-29 19:37:50 -0500

    Opcion para borrar ToDos

--- 09-hook-app/src/components/08-useReducer/TodoApp.js
+++ 09-hook-app/src/components/08-useReducer/TodoApp.js
@@ -20,6 +20,14 @@ export const TodoApp = () => {
         localStorage.setItem('todos', JSON.stringify( todos ));
     }, [todos]);
 
+    const handleDelete = ( todoId ) => {
+        const action = {
+            type: 'delete',
+            payload: todoId
+        };
+        dispatch(action);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -56,7 +64,11 @@ export const TodoApp = () => {
                                 className="list-group-item"
                                 >
                                     <p className="text-center">{i + 1}. {todo.desc}</p>
-                                    <button className="btn btn-danger">
+
+                                    <button 
+                                        className="btn btn-danger"
+                                        onClick={() => {handleDelete(todo.id)}}
+                                    >
                                         Borrar
                                     </button>
                                 </li>
