0bbcd92d1a1d5c4e28e103b180923948e7d60af0
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-02 13:30:50 -0600

    Configurando el nuevo proyecto con ReactRouter

--- 09-hook-app/src/index.js
+++ 09-hook-app/src/index.js
@@ -14,10 +14,11 @@ import ReactDOM from 'react-dom';
 // // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // // import { CounterApp } from './components/01-useState/CounterApp';
 // // import { HookApp } from './HookApp';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 
 ReactDOM.render(
-    <TodoApp />,
+    <MainApp />,
   document.getElementById('root')
 );
 
