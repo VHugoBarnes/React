90ddc80fa20fe347c638c0eb59e70de92c35cebf
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-02 16:27:10 -0600

    CreateContext y useContext

--- 09-hook-app/src/components/09-useContext/HomeScreen.js
+++ 09-hook-app/src/components/09-useContext/HomeScreen.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomeScreen = () => {
+
+    const userContext = useContext(UserContext);
+    console.log(userContext);
+
     return (
         <div>
             <h1 className="mt-3 ml-3">HomeScreen</h1>
