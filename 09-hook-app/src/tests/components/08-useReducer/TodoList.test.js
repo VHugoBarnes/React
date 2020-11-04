1eefa943766ad0a42b6989839a931a9d75064ad0
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-04 14:38:44 -0600

    Pruebas en TodoList

---
+++ 09-hook-app/src/tests/components/08-useReducer/TodoList.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+
+import { TodoList } from '../../../components/08-useReducer/TodoList';
+import { demoTodos } from '../../fixtures/demoTodos';
+
+
+describe('Pruebas en <TodoList />', () => {
+
+    const mockHandleDelete = jest.fn();
+    const mockHandleToggle = jest.fn();
+    const wrapper = shallow(
+        <TodoList 
+            todos={ demoTodos }
+            handleDelete={ mockHandleDelete }
+            handleToggle={ mockHandleToggle }
+        />);
+   
+    test('Debe de mostrarse correctamente', () => {
+        expect( wrapper ).toMatchSnapshot();
+    });
+
+    test('Debe de tener dos <TodoListItem/>', () => {
+        expect( wrapper.find('TodoListItem').length ).toBe( demoTodos.length );
+        console.log( wrapper.find( 'TodoListItem' ).at(0).props() );
+    });
+    
+});
