cb80ceb4611b97ad48ac72e4b40533088ce2e4eb
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-06 15:28:10 -0600

    History push/replace

--- 14-heroes-app/src/components/login/LoginScreen.js
+++ 14-heroes-app/src/components/login/LoginScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+
+    const handleLogin = () => {
+        // Reemplaza en la historia la página por la ruta especificadag
+        history.replace('/');
+    };
+
     return (
-        <>
-            <h1>Login Screen</h1>
-            
-        </>
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr/>
+            <button 
+                className="btn btn-primary"
+                onClick={ handleLogin }
+            >
+                Login
+            </button>
+        </div>
     );
 };
