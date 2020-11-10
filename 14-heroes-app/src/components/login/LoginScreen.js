113ae356ab63846052246eb8dc0d93741d39aac1
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-10 15:13:52 -0600

    Login del usuario

--- 14-heroes-app/src/components/login/LoginScreen.js
+++ 14-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext(AuthContext);
+
     const handleLogin = () => {
         // Reemplaza en la historia la página por la ruta especificada
+        // history.replace('/');
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Víctor'
+            }
+        })
         history.replace('/');
     };
 
