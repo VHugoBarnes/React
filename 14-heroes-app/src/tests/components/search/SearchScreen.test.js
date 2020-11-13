ce8ba0145320de6f2c958688022570fd6010c926
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-12 23:45:46 -0600

    Pruebas faltantes del componente SearchScreen

--- 14-heroes-app/src/tests/components/search/SearchScreen.test.js
+++ 14-heroes-app/src/tests/components/search/SearchScreen.test.js
@@ -29,4 +29,42 @@ describe('Pruebas en <SearchScreen/>', () => {
         expect(wrapper).toMatchSnapshot();
     });
     
+    test('Debe de mostrar un error si no se encuentra el Hero', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman11']}>
+                <Route path="/search" component={SearchScreen}/>
+            </MemoryRouter>
+        );
+        expect(wrapper.find('input').prop('value')).toBe('batman11');
+        expect(wrapper.find('.alert-danger').text().trim()).toBe(`There's no hero with batman11`);
+        expect(wrapper.find('.alert-danger').exists()).toBeTruthy();
+    });
+
+    test('Debe de llamar el push del history', () => {
+        const historyMock = {
+            push: jest.fn(),
+        };
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman11']}>
+                <Route 
+                    path="/search" 
+                    component={(props) => <SearchScreen history={historyMock}/>}/>
+            </MemoryRouter>
+        );
+
+        wrapper.find('input').simulate('change',{
+            target: {
+                name: 'search',
+                value: 'batman',
+            }
+        });
+
+        wrapper.find('form').prop('onSubmit')({
+            preventDefault(){}
+        });
+
+        expect(historyMock.push).toHaveBeenCalledWith(`?q=batman`);
+    });
+    
+    
 });
