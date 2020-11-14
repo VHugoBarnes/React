c6d83cbaf996863d7a831adc7b910affd0867201
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-13 23:52:42 -0600

    Componente cuando no haya nada seleccionado

---
+++ 17-journal-app/src/components/journal/NothingSelected.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const NothingSelected = () => {
+    return (
+        <div className="nothing__main-content">
+            <p>
+                Select a entry
+                <br/>
+                or create one
+            </p>
+            <i className="far fa-star fa-4x mt-5"></i>
+        </div>
+    );
+};
