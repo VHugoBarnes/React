4f2aeee05d0a0d33d80a060fd8b9ac738c40da5a
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-14 19:41:04 -0600

    Estructura y diseño del homescreen

--- 17-journal-app/src/components/notes/NoteScreen.js
+++ 17-journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,27 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+            <div className="notes__content">
+                <input 
+                    type="text"
+                    placeholder="Title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea 
+                    placeholder="What happened today?"
+                    className="notes__text-area"
+                    autoComplete="off"
+                >
+                </textarea>
+                <div className="notes__image">
+                    <img 
+                        src="https://c.files.bbci.co.uk/92F7/production/_101232673_angry.jpg"
+                        alt="image"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
