f43cca80cfa01210969dc34fc60f31893e77949d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-18 12:13:44 -0600

    Mostrando un loading global en la aplicacion

--- 17-journal-app/src/routers/AppRouter.js
+++ 17-journal-app/src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
     Redirect, 
     Route, 
@@ -16,13 +16,35 @@ export const AppRouter = () => {
 
     const dispatch = useDispatch();
 
+    // Con esto revisamos la sesión.
+    const [checking, setChecking] = useState(true);
+    // Revisamos si está autenticado.
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     useEffect(() => {
+        // Manteniendo la sesión aunque se recargue el navegador
+        // Prácticamente, este es un observable que está atento
+        // a si está autenticado el usuario. Si es así, 
+        // y si se encuentra el usuario, se dispara el dispatch
+        // para hacer login.
         firebase.auth().onAuthStateChanged( (user) => {
+            // IMPORTANTE RECORDAR: La sintaxis '?' se refiere a si
+            // el valor de la variable no es undefined, procede a obtener
+            // el valor después del punto.
             if(user?.uid) {
                 dispatch( login(user.uid, user.displayName) );
+                setIsLoggedIn( true );
+            } else {
+                setIsLoggedIn( false );
             }
+            setChecking(false);
         })
-    }, [dispatch]);
+    }, [ dispatch, setChecking, setIsLoggedIn ]);
+
+    // Loading global de la aplicación
+    if(checking) {
+        return(<h1>Espere...</h1>)
+    }
 
     return (
         <Router>
