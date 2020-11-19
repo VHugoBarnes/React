15fd4b0a760b366034ace8981942d47e7f00c0ec
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-18 23:45:35 -0600

    NotesReducer

--- 17-journal-app/src/routers/AppRouter.js
+++ 17-journal-app/src/routers/AppRouter.js
@@ -44,7 +44,7 @@ export const AppRouter = () => {
 
     // Loading global de la aplicación
     if(checking) {
-        return(<h1>Espere...</h1>)
+        return(<h1>Wait...</h1>)
     }
 
     return (
