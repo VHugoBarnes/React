223f33d069051f4d0e5895b2c7f93b29c7408143
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-20 14:47:25 -0600

    Actualizar el panel lateral y mensajes de grabacion

--- 17-journal-app/src/actions/notes.js
+++ 17-journal-app/src/actions/notes.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { db } from "../firebase/firebaseConfig";
 import { loadNotes } from "../helpers/loadNotes";
 import { types } from "../types/types";
@@ -55,5 +56,16 @@ export const startSaveNote = ( note ) => {
         delete noteToFirestore.id;
 
         await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+
+        dispatch(refreshNote(note.id, noteToFirestore));
+        Swal.fire('Saved', note.title, 'success');
     }
 }
+
+export const refreshNote = (id, note) => ({
+    type: types.notesUpdate,
+    payload: {
+        id, 
+        note: {id, ...note}
+    }
+})
