e44abe0f972ea8cf0e923b919a3c080ac8f75e1c
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-24 17:28:42 -0600

    Pruebas en acciones sincronas

---
+++ 17-journal-app/src/tests/actions/ui.test.js
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+import { finishLoading, removeError, setError, startLoading } from '../../actions/ui';
+import { types } from '../../types/types';
+
+describe('Pruebas en Ui-Actions', () => {
+    
+    test('Todas las acciones deben de funcionar', () => {
+        const setErrorAction = setError('Help!!');
+        const removeErrorAction = removeError();
+        const startLoadingAction = startLoading();
+        const finishLoadingAction = finishLoading();
+
+        expect(setErrorAction).toEqual({
+            type: types.uiSetError,
+            payload: 'Help!!',
+        });
+
+        expect(removeErrorAction).toEqual({
+            type: types.uiRemoveError,
+        });
+
+        expect(startLoadingAction).toEqual({
+            type: types.uiStartLoading,
+        });
+
+        expect(finishLoadingAction).toEqual({
+            type: types.uiFinishLoading,
+        });
+    });
+    
+});
