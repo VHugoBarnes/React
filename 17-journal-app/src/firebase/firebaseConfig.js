cbe7f902cefb1985c1e08874f611b2533d0542b1
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-25 15:20:25 -0600

    Crear base de datos de desarrollo

--- 17-journal-app/src/firebase/firebaseConfig.js
+++ 17-journal-app/src/firebase/firebaseConfig.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 // Web app firebase configuration
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyC9p-msd3oc63MfzH5w_JB7xcYtJkgNZHc",
     authDomain: "react-app-cursos-ebd43.firebaseapp.com",
     databaseURL: "https://react-app-cursos-ebd43.firebaseio.com",
@@ -13,8 +13,26 @@ var firebaseConfig = {
     appId: "1:243161818857:web:5b05833882195373ffb278"
 };
 
+const firebaseConfigTesting = {
+    apiKey: "AIzaSyAhJMJ9mzQU-p_PGeimCMyFub_DVLgQzSg",
+    authDomain: "testing-redux.firebaseapp.com",
+    databaseURL: "https://testing-redux.firebaseio.com",
+    projectId: "testing-redux",
+    storageBucket: "testing-redux.appspot.com",
+    messagingSenderId: "841499302567",
+    appId: "1:841499302567:web:cae16d37c0f31d30ffd982"
+  };
+
+// Preguntamos si el ambiente es de Testing
+if(process.env.NODE_ENV === 'test') {
+    // Testing
+    firebase.initializeApp(firebaseConfigTesting);
+} else {
+    // dev-prod
+    firebase.initializeApp(firebaseConfig);
+}
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 // Database reference
 const db = firebase.firestore();
--- 17-journal-app/src/tests/actions/notes.test.js
+++ 17-journal-app/src/tests/actions/notes.test.js
@@ -1,7 +1,10 @@
 import '@testing-library/jest-dom';
 import configureStore from 'redux-mock-store'; //ES6 modules
 import thunk from 'redux-thunk';
+
+import { db } from "../../firebase/firebaseConfig";
 import { startNewNote } from '../../actions/notes';
+import { types } from '../../types/types';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -17,6 +20,32 @@ describe('Pruebas en notes - Action', () => {
    
     test('Debe de crear una nueva nota startNewNote', async() => {
         await store.dispatch( startNewNote() );
+        // Contiene un arreglo [] con las acciones llamadas. En este caso,
+        // activeNote y addNewNote
+        const actions = store.getActions();
+        
+        expect(actions[0]).toEqual({
+            type: types.notesActive,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number),
+            }
+        });
+
+        expect(actions[1]).toEqual({
+            type: types.notesAddNew,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number),
+            }
+        });
+
+        const id = actions[0].payload.id;
+        await db.doc(`TESTING/journal/notes/${id}`).delete();
     });
     
 });
