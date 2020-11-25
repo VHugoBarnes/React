361db6cf46c5a0166c4c9b00ea28536630bb1f0d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-25 16:45:21 -0600

    Creando las variables de entorno

--- 17-journal-app/src/firebase/firebaseConfig.js
+++ 17-journal-app/src/firebase/firebaseConfig.js
@@ -2,38 +2,21 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// Web app firebase configuration
+// console.log( process.env );
+
+// Web app firebase configuration for dev/prod
 const firebaseConfig = {
-    apiKey: "AIzaSyC9p-msd3oc63MfzH5w_JB7xcYtJkgNZHc",
-    authDomain: "react-app-cursos-ebd43.firebaseapp.com",
-    databaseURL: "https://react-app-cursos-ebd43.firebaseio.com",
-    projectId: "react-app-cursos-ebd43",
-    storageBucket: "react-app-cursos-ebd43.appspot.com",
-    messagingSenderId: "243161818857",
-    appId: "1:243161818857:web:5b05833882195373ffb278"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
-const firebaseConfigTesting = {
-    apiKey: "AIzaSyAhJMJ9mzQU-p_PGeimCMyFub_DVLgQzSg",
-    authDomain: "testing-redux.firebaseapp.com",
-    databaseURL: "https://testing-redux.firebaseio.com",
-    projectId: "testing-redux",
-    storageBucket: "testing-redux.appspot.com",
-    messagingSenderId: "841499302567",
-    appId: "1:841499302567:web:cae16d37c0f31d30ffd982"
-  };
-
-// Preguntamos si el ambiente es de Testing
-if(process.env.NODE_ENV === 'test') {
-    // Testing
-    firebase.initializeApp(firebaseConfigTesting);
-} else {
-    // dev-prod
-    firebase.initializeApp(firebaseConfig);
-}
-
 // Initialize Firebase
-
+firebase.initializeApp(firebaseConfig);
 // Database reference
 const db = firebase.firestore();
 // Google auth reference
