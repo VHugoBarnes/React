dfa6d44779d29d00707b8d3d30f314f6175c6c3b
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-28 17:56:42 -0600

    CalendarReducer y primeras acciones con los eventos2

--- 22-calendar-app/src/components/calendar/CalendarScreen.js
+++ 22-calendar-app/src/components/calendar/CalendarScreen.js
@@ -44,6 +44,7 @@ export const CalendarScreen = () => {
 
     const onSelectEvent = (e) => {
         dispatch(eventSetActive(e));
+        dispatch(uiOpenModal());
     }
 
     const onViewChange = (e) => {
