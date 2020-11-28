a310083e671e1166371de34227c9eaef784585a7
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-28 12:55:22 -0600

    Configuraciones adicionales al calendario

--- 22-calendar-app/src/components/calendar/CalendarScreen.js
+++ 22-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import {Calendar, momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
+import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-esp';
 
+moment.locale('es');
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
 const myEventsList = [{
     title: 'Cumpleaños de Keko',
     start: moment().toDate(),    // new Date()
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel',
 }];
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        console.log(event, start, end, isSelected);
+        const style = {
+            backgroundColor: '#367cf7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <div className="calendar-screen">
             <Navbar/>
@@ -22,6 +42,8 @@ export const CalendarScreen = () => {
                 events={myEventsList}
                 startAccessor="start"
                 endAccessor="end"
+                messages={messages}
+                eventPropGetter={ eventStyleGetter }
             />  
         </div>
     )
