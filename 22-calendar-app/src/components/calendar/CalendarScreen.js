9681950a26e50a0cb112ab8db42938c492a30b36
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-11-27 20:15:46 -0600

    Implementando React-Big-Calendar

--- 22-calendar-app/src/components/calendar/CalendarScreen.js
+++ 22-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 import { Navbar } from '../ui/Navbar';
+
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+const myEventsList = [{
+    title: 'Cumpleaños de Keko',
+    start: moment().toDate(),    // new Date()
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+}];
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className="calendar-screen">
             <Navbar/>
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+            />  
         </div>
     )
 }
