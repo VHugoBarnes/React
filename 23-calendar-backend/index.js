8646ef46ce8c76822df125c018d2364678cdc8cc
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-12-01 17:27:07 -0600

    Configurando Express

--- 23-calendar-backend/index.js
+++ 23-calendar-backend/index.js
@@ -1,1 +1,16 @@
-console.log('Hola Mundo');
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+    console.log('Server en puerto 4000');
+});
