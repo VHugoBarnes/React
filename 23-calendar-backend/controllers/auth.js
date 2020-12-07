b1b6416dde87761f21f5fff2ea4ad353e7787982
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-12-07 14:43:37 -0600

    Validaciones del usuario con Mongoose

--- 23-calendar-backend/controllers/auth.js
+++ 23-calendar-backend/controllers/auth.js
@@ -3,13 +3,25 @@ const Usuario = require('../models/Usuario');
 
 const createUser = async(req, res = express.response ) => {
 
+    const { email, usuario } = req.body;
+
     try {
-        const usuario = new Usuario(req.body);
+        let usuario = await Usuario.findOne({email});
+        
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese correo',
+            });
+        }
+
+        usuario = new Usuario(req.body);
         await usuario.save();
 
         res.status(201).json({
             ok: true,
-            msg: 'register',
+            uid: usuario.id,
+            name: usuario.name
         });
     } catch (error) {
         res.status(500).json({
