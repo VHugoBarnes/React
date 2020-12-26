ef96ad208c8a5ff4e48908616e76f513bfc8e015
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-12-26 15:58:32 -0600

    Pruebas con el helper FetchConToken

--- 22-calendar-app/src/tests/helpers/fetch.test.js
+++ 22-calendar-app/src/tests/helpers/fetch.test.js
@@ -3,6 +3,8 @@ import { fetchConToken, fetchSinToken } from '../../helpers/fetch';
 
 describe('Pruebas en el helper fetch', () => {
    
+    let token = '';
+
     test('fetchSinToken debe de funcionar', async() => {
         
         const resp = await fetchSinToken('auth', {email: 'test@mail.com', password: '123456'}, 'POST');
@@ -11,6 +13,17 @@ describe('Pruebas en el helper fetch', () => {
         const body = await resp.json();
         expect( body.ok ).toBe(true);
 
+        token = body.token;
+
+    });
+
+    test('fetchConToken debe de funcionar', async() => {
+        localStorage.setItem('token', token);
+        const resp = await fetchConToken('events/5fd2e75d5e062eb0ccd7e887', {}, 'DELETE');
+
+        const body = await resp.json();
+
+        expect(body.msg).toBe('Evento no exite por ese Id');
     });
     
 });
