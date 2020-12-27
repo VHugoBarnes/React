1b8bf45369c75404e20b0bd0b78b19e78c82e591
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-12-27 17:21:54 -0600

    Pruebas con el uiReducer

---
+++ 22-calendar-app/src/tests/reducers/uiReducer.test.js
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom';
+import { uiCloseModal, uiOpenModal } from '../../actions/ui';
+import { uiReducer } from '../../reducers/uiReducer';
+
+const initialState = {
+    modalOpen: false
+};
+
+describe('Pruebas en uiReducer', () => {
+    
+    test('Debe de retornar el estado por defecto', () => {
+        const state = uiReducer( initialState, {} );
+        expect( state ).toEqual(initialState);
+    });
+
+    test('Debe de abrir y cerrar el modal', () => {
+        const modalOpen = uiOpenModal();
+        const state = uiReducer(initialState, modalOpen);
+        expect( state ).toEqual({modalOpen: true});
+
+        const modalClose = uiCloseModal();
+        const stateClose = uiReducer(state, modalClose);
+        expect( stateClose ).toEqual({modalOpen: false});
+    });
+    
+});
