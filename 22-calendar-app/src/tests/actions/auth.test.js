118e9f54ebc975084d4af0d1cff7a242c841bf7f
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-12-27 16:43:35 -0600

    Pruebas con el registro de usuarios

--- 22-calendar-app/src/tests/actions/auth.test.js
+++ 22-calendar-app/src/tests/actions/auth.test.js
@@ -2,8 +2,9 @@ import '@testing-library/jest-dom';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import Swal from 'sweetalert2';
-import { startLogin } from '../../actions/auth';
+import { startLogin, startRegister } from '../../actions/auth';
 import { types } from '../../types/types';
+import * as fetchModule from '../../helpers/fetch';
 
 jest.mock('sweetalert2', () => ({
     fire: jest.fn(),
@@ -53,6 +54,32 @@ describe('Pruebas en las acciones auth', () => {
         actions = store.getActions();
         expect( actions ).toEqual([]);
         expect( Swal.fire ).toHaveBeenCalledWith('Error', 'El usuario no existe con ese email', 'error');
+    });
+    
+    test('startRegister correcto', async() => {
+        fetchModule.fetchSinToken = jest.fn(() => ({
+            json() {
+                return {
+                    ok: true,
+                    uid: '123',
+                    name: 'Keko',
+                    token: 'ABC123ABC123'
+                }
+            }
+        }));
+        await store.dispatch( startRegister('test1@test.com', '123456', 'Testi') );
+
+        const actions = store.getActions();
+        expect( actions[0] ).toEqual({
+            type: types.authLogin,
+            payload: {
+                uid: '123',
+                name: 'Keko',
+
+            }
+        });
+        expect( localStorage.setItem ).toHaveBeenCalledWith('token', 'ABC123ABC123');
+        expect( localStorage.setItem ).toHaveBeenCalledWith('token-init-date', expect.any(Number));
     });
     
 });
